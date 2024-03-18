2b06f8a1f52881821073b80d256c73c61705a384
KOUSTAVSAMANTA <koustavsamanta007@gmail.com>
2024-03-18 21:34:54 +0530

    lots of update

--- src/components/ChatScreen.jsx
+++ src/components/ChatScreen.jsx
@@ -69,37 +69,79 @@ const ChatScreen = () => {
       Pratyush: ''
     }
   }
+  let preprocess = []
   try {
     // tms = {Pratyush:meta_data.Pratyush.timestamp.toDate().toUTCString().split(" "),
     //           [userName]:meta_data[userName].timestamp.toDate().toUTCString().split(" ")}
     // // meta_data[["timestamp"] = tms[tms.length - 2]]
-    let p_time = Date(meta_data.Pratyush.timestamp)
-    let s_time = Date(meta_data[userName[0]].timestamp)
-    tms['Pratyush'] = p_time.split(" ")[p_time.split(" ").length-5]
-    tms[userName[0]] = s_time.split(" ")[s_time.split(" ").length-5]
-    tms["full_timestamp"]['Pratyush'] = p_time.replace("(India Standard Time)",'')
-    tms["full_timestamp"][userName[0]] = s_time.replace("(India Standard Time)",'')
+    let tms_p = meta_data.Pratyush.timestamp
+    let msg_p = meta_data.Pratyush.messages
+    for (let i = 0; i < tms_p.length; i++) {
+      // Your code inside the loop here
+      console.log("messages",msg_p[i]);
+      if(msg[tms_p[i].toDate()]){
+        msg[tms_p[i].toDate()].Pratyush[0].push(msg_p[i])
+      }else{
+        msg[tms_p[i].toDate()] = {Pratyush:[[msg_p[i]],tms_p[i].toDate().toUTCString()]}
+      }
+    }
+    
+    
+    console.log("msg",msg)
+    const _pdateList = tms_p.map(tms_p => tms_p.toDate());
+
+    let tms_s = meta_data[userName[0]].timestamp
+    let msg_s = meta_data[userName[0]].messages
+    for (let i = 0; i < tms_s.length; i++) {
+      // Your code inside the loop here
+      console.log("messages",msg_s[i]);
+      if(msg[tms_s[i].toDate()]){
+        msg[tms_s[i].toDate()][userName[0]][0].push(msg_s[i])
+      }else{
+        msg[tms_s[i].toDate()] = {[userName[0]]:[[msg_s[i]],tms_s[i].toDate().toUTCString()]}
+      }
+    }
+
+
+    const _sateList = tms_s.map(tms => tms.toDate());
+
+    // merged tms
+    const mergedSet = new Set([..._pdateList, ..._sateList]);
+    const mergedDates = Array.from(mergedSet).sort((a, b) => a - b);
+
+    let p_time = Date(meta_data.Pratyush.timestamp[0])
+    let s_time = Date(meta_data[userName[0]].timestamp[0])
+    tms['Pratyush'] = p_time.split(" ")[p_time.split(" ").length - 5]
+    tms[userName[0]] = s_time.split(" ")[s_time.split(" ").length - 5]
+    tms["full_timestamp"]['Pratyush'] = p_time.replace("(India Standard Time)", '')
+    tms["full_timestamp"][userName[0]] = s_time.replace("(India Standard Time)", '')
     // msg = {Pratyush:meta_data.Pratyush.messages,
     //       [userName]:meta_data[userName].messages,}
     // isSender = meta_data.username[1]
-    console.log("Hello i am meta Data", tms)
+    console.log("Hello i am meta Data", _pdateList)
+    console.log("tms_s", mergedDates)
+    let mappedDATA = mergedDates.map(tms => msg[tms]);
+    preprocess = mappedDATA
+    console.log("preprocessed",preprocess)
+
 
   } catch {
     console.log("error")
   }
 
 
-  console.log(meta_data)
-  // const messageElements = msg.map((message, index) => (
+  console.log("outloop",preprocess)
+  const messageElements = preprocess.map((message, index) => (
+    // console.log(Boolean.parseBoolean(Object.keys(message)[0]))
 
-  //   <div className={`message_body ${isSender? 'message_bodySender':''}`}key={index}>
-  //     <p className="chat_name">{isSender? isSender:data1.username[0]}</p>
-  //     <p key={index} className="chat_message">
-  //       {message}
-  //       <span className="chat_Timestamp">{meta_data.timestamp}</span>
-  //     </p>
-  //   </div>
-  // ));
+    <div className={`message_body ${((Object.keys(message)[0])==='Pratyush')? 'message_bodySender':''}`}key={index}>
+      <p className="chat_name">{(Boolean(Object.keys(message)[0])==='Pratyush')? (Boolean(Object.keys(message)[0])==='Pratyush'):Object.keys(message)[0]}</p>
+      <p key={index} className="chat_message">
+        {message[Object.keys(message)[0]][0]}
+        <span className="chat_Timestamp">{message[Object.keys(message)[0]][1].split(' ')[message[Object.keys(message)[0]][1].split(' ').length-2]}</span>
+      </p>
+    </div>
+  ));
 
 
 
@@ -123,6 +165,8 @@ const ChatScreen = () => {
 
       </div>
       <div className="chat__body">
+    
+        {messageElements}
 
 
       </div>
